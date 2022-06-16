557fa014f426b1104a6af055396bb312c4e23b06
Bohdan Makohin <ajukraine@gmail.com>
2022-06-17 00:49:05 +0300

    feat(options): show <Tab> character as 2 spaces

--- shared/lua/aj/config.lua
+++ shared/lua/aj/config.lua
@@ -56,6 +56,7 @@ o.whichwrap = '<,>,[,]'
 o.expandtab = true
 o.shiftwidth = 2
 o.softtabstop = 2
+o.tabstop = 2
 
 -- Always show some lines below and above the cursor when scrolling
 o.scrolloff = 5
