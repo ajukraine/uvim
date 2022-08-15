768f17672d17f934717e676f04435a9bdac9eb46
Bohdan Makohin <ajukraine@gmail.com>
2022-08-15 12:28:28 +0300

    refactor(init.lua): make the config flow more laconic and visible

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,12 +1,3 @@
-vim.o.compatible = false
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = vim.fn.expand('$MYVIMRC'),
-  nested = true,
-  command = "source <afile>",
-  group = vim.api.nvim_create_augroup("config_reload", { clear = true })
-})
-
 local function bootstrap_vimplug()
   local vimplug_install_path = vim.fn.stdpath('data') .. '/site/autoload/plug.vim'
 
@@ -16,21 +7,12 @@ local function bootstrap_vimplug()
   end
 end
 
-package.loaded['aj.config'] = nil
-local opts = {
-  is_nvim = true,
-  is_vim = false,
-  has_guicolors = vim.fn.has('termguicolors') == 1
-}
-
-local config = require('aj.config').get_config(opts)
-
 local function configure_plugins(plugins)
   local Plug = vim.fn['plug#']
 
   vim.call('plug#begin')
 
-  for i, plugin in ipairs(config.plugins) do
+  for i, plugin in ipairs(plugins) do
     local opts = {}
     if type(plugin) == type({}) then
       opts = plugin
@@ -47,8 +29,6 @@ local function configure_plugins(plugins)
   vim.call('plug#end')
 end
 
-configure_plugins(config.plugins)
-
 local function configure_options(options)
   for name, value in pairs(options) do
     if vim.fn.exists('+' .. name) == 0 then
@@ -60,23 +40,17 @@ local function configure_options(options)
   end
 end
 
-configure_options(config.options)
-
 local function configure_globals(globals)
   for name, value in pairs(globals) do
     vim.g[name] = value
   end
 end
 
-configure_globals(config.globals)
-
-local function trigger_hook(hook)
-  for i, action in ipairs(config.hooks[hook]) do
+local function trigger_hook(hooks, hook)
+  for i, action in ipairs(hooks[hook]) do
     action()
   end
 end
-
-trigger_hook('PostPlugins')
 
 local function configure_mappings(mappings)
   local mappers = {
@@ -93,13 +67,38 @@ local function configure_mappings(mappings)
   end
 end
 
+local function configure_custom_options(custom_options)
+  if custom_options.transparent_background then
+    vim.cmd [[autocmd! Colorscheme * hi Normal guibg=NONE ctermbg=NONE]]
+  end
+
+  if custom_options.colorscheme then
+    vim.cmd([[colorscheme ]] .. custom_options.colorscheme)
+  end
+end
+
+vim.o.compatible = false
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = vim.fn.expand('$MYVIMRC'),
+  nested = true,
+  command = "source <afile>",
+  group = vim.api.nvim_create_augroup("config_reload", { clear = true })
+})
+
+local opts = {
+  is_nvim = true,
+  is_vim = false,
+  has_guicolors = vim.fn.has('termguicolors') == 1
+}
+
+package.loaded['aj.config'] = nil
+local config = require('aj.config').get_config(opts)
+
+bootstrap_vimplug()
+configure_options(config.options)
+configure_globals(config.globals)
+configure_plugins(config.plugins)
+trigger_hook(config.hooks, 'PostPlugins')
 configure_mappings(config.mappings)
-
--- Make background transparent after color scheme is changed
-if config.custom_options.transparent_background then
-  vim.cmd [[autocmd! Colorscheme * hi Normal guibg=NONE ctermbg=NONE]]
-end
-
-if config.custom_options.colorscheme then
-  vim.cmd([[colorscheme ]] .. config.custom_options.colorscheme)
-end
+configure_custom_options(config.custom_options)
