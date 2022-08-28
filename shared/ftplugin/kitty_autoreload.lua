f37dffeeb55837bd98083a4669ec294c761a86f6
Bohdan Makohin <ajukraine@gmail.com>
2022-08-28 19:20:15 +0300

    chore(kitty_autoreload): present idea of how future feature definition could look like

--- shared/ftplugin/kitty_autoreload.lua
+++ shared/ftplugin/kitty_autoreload.lua
@@ -1,3 +1,5 @@
+-- WARNING: Supports only Neovim
+
 if vim.b.did_kitty_autoreload then return -1 end
 if vim.fn.executable('killall') ~= 1 then return -1 end
 
@@ -7,3 +9,10 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 })
 
 vim.b.did_kitty_autoreload = true
+
+-- Idea of possible of feature definition
+-- require 'aj.feature'
+--   .depends_on('killall')
+--   .setup(function ()
+--     return au('BufWritePost', 'silent !killall -SIGUSR1 kitty', { buffer = true })
+--   end)
