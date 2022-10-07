3d5d48fb9ccf9406849c5f55b23955333f30e986
Bohdan Makohin <ajukraine@gmail.com>
2022-10-07 22:27:45 +0300

    test(startup): add colors(bright black) to benchmark hook

--- test/bench.sh
+++ test/bench.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo 'Running vim-startuptime...'
+printf "%b" "\e[90mRunning vim-startuptime...\e[0m\n"
 vim-startuptime -warmup 1 -count 10 -vimpath nvim |
   awk '
   BEGIN { FS=" *" }
   /^Total Average/{ time=$3; exit }
   END {
-    print "Total avg time: " time "ms"
+    print "\033[90m" "Total avg time: " time "ms" "\033[0m"
     if (time > 100) {
-      print "\033[31m" "error: startup time exceeds 100ms"
+      print "\033[31m" "error: startup time exceeds 100ms" "\033[0m"
       exit 1
     } 
   }'
